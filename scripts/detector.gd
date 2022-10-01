6c87713ebe6993cbe6752185f4be2bcf1f480a84
pizzart <7o7goo7o7@gmail.com>
2022-10-01 16:08:10 +0300

    raycasts

--- scripts/detector.gd
+++ scripts/detector.gd
@@ -1,21 +1,31 @@
 extends Spatial
 
 var player: Spatial
-onready var ray = $RayCast
+onready var ray1 = $Ray1
+onready var ray2 = $Ray2
+onready var ray3 = $Ray3
+onready var rays = [ray1, ray2, ray3]
 
 func _ready():
 	player = get_tree().get_nodes_in_group("player")[0]
 	Global.connect("timeout", self, "_on_timeout")
 
 func _physics_process(delta):
-	ray.cast_to = to_local(player.transform.origin)
-	ray.force_raycast_update()
+	var dir = transform.origin.direction_to(player.transform.origin)
+	var dir_l = transform.origin.direction_to(player.transform.origin + dir.rotated(Vector3.UP, PI / 2) / 2)
+	var dir_r = transform.origin.direction_to(player.transform.origin + dir.rotated(Vector3.UP, -PI / 2) / 2)
+	var dist = transform.origin.distance_to(player.transform.origin)
+	$Mesh.look_at(player.transform.origin, Vector3.UP)
+	ray1.cast_to = dir * dist
+	ray2.cast_to = dir_l * dist
+	ray3.cast_to = dir_r * dist
 
 func _on_timeout():
 	$Light.show()
 	yield(get_tree().create_timer(0.5), "timeout")
-	if ray.is_colliding():
-		if ray.get_collider() is KinematicBody:
-			get_tree().quit()
-			return
+	for ray in rays:
+		if ray.is_colliding():
+			if ray.get_collider() is KinematicBody:
+				get_tree().quit()
+				return
 	$Light.hide()
