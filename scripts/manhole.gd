900eeea69cb0cdcff3def914345e2ce3f1e1c592
pizzart <7o7goo7o7@gmail.com>
2022-10-02 19:47:40 +0300

    manhole yo

--- scripts/manhole.gd
+++ scripts/manhole.gd
@@ -1,6 +1,28 @@
 extends StaticBody
 
+var hidden: bool
 onready var player = get_tree().get_nodes_in_group("player")[0]
 
+func _input(event: InputEvent):
+	if event.is_action_pressed("action"):
+		if not $Mesh/AnimationPlayer.is_playing():
+			if player in $Area.get_overlapping_bodies() and not hidden:
+				enter()
+			elif hidden:
+				exit()
+
 func enter():
+	if player.hide_in_manhole(self):
+		var dir = global_translation.direction_to(player.global_translation)
+		$Mesh.rotation.y = atan2(-dir.z, dir.x) - PI / 2
+		$Mesh/AnimationPlayer.play("open")
+		var tween = create_tween()
+		tween.tween_property($Prompt, "modulate", Color("#44ffffff"), 0.5)
+		hidden = true
+
+func exit():
+	player.unmanhole()
 	$Mesh/AnimationPlayer.play("open")
+	var tween = create_tween()
+	tween.tween_property($Prompt, "modulate", Color.white, 0.5)
+	hidden = false
--- scripts/player.gd
+++ scripts/player.gd
@@ -12,6 +12,7 @@ var pushing_speed: float
 var pushing_timer: float
 var barrel_mode: bool
 var barrel_timeout: float = 0.1
+var manholed: bool
 onready var camera_init_pos = $Camera.translation
 onready var camera_init_fov = $Camera.fov
 
@@ -24,49 +25,50 @@ func _ready():
 	Global.connect("timeout", self, "_on_timeout")
 
 func _physics_process(delta: float):
-	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	direction = input_vector
-	if not barrel_mode:
-		if pushing:
-			pushing_speed = lerp(pushing_speed, INERTIA, delta * 5)
-			velocity = Vector3(direction.x, 0, direction.y) * pushing_speed
+	if not manholed:
+		var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		direction = input_vector
+		if not barrel_mode:
+			if pushing:
+				pushing_speed = lerp(pushing_speed, INERTIA, delta * 5)
+				velocity = Vector3(direction.x, 0, direction.y) * pushing_speed
+			else:
+				pushing_speed = lerp(pushing_speed, 0, delta * 10)
+				velocity = velocity.linear_interpolate(Vector3(direction.x, 0, direction.y) * SPEED, delta * 10)
 		else:
-			pushing_speed = lerp(pushing_speed, 0, delta * 10)
-			velocity = velocity.linear_interpolate(Vector3(direction.x, 0, direction.y) * SPEED, delta * 10)
-	else:
-		velocity = velocity.linear_interpolate(Vector3(direction.x, 0, direction.y) * BARREL_SPEED, delta)
-		if barrel_timeout > 0:
-			barrel_timeout -= delta
-		$BarrelMesh.rotate_x(-velocity.length() / 60)
-	velocity.y -= 1
-	velocity = move_and_slide(velocity, Vector3.UP, false, 4, PI / 4, false)
+			velocity = velocity.linear_interpolate(Vector3(direction.x, 0, direction.y) * BARREL_SPEED, delta)
+			if barrel_timeout > 0:
+				barrel_timeout -= delta
+			$BarrelMesh.rotate_x(-velocity.length() / 60)
+		velocity.y -= 1
+		velocity = move_and_slide(velocity, Vector3.UP, false, 4, PI / 4, false)
 
-	var pushed = false
-	for i in get_slide_count():
-		var col = get_slide_collision(i)
-		if col.collider.is_in_group("box"):
-			col.collider.add_velo(-col.normal * pushing_speed)
-			pushed = true
+		var pushed = false
+		for i in get_slide_count():
+			var col = get_slide_collision(i)
+			if col.collider.is_in_group("box"):
+				col.collider.add_velo(-col.normal * pushing_speed)
+				pushed = true
 
-	if not pushed:
-		pushing_timer += delta
-		if pushing_timer > 0.1:
-			pushing = false
+		if not pushed:
+			pushing_timer += delta
+			if pushing_timer > 0.1:
+				pushing = false
+				pushing_timer = 0
+		else:
+			pushing = true
 			pushing_timer = 0
-	else:
-		pushing = true
-		pushing_timer = 0
-	
-	$Camera.transform.origin = $Camera.transform.origin.linear_interpolate(transform.origin + camera_init_pos, delta * 15)
-	if direction:
-		rotation.y = lerp_angle(rotation.y, Vector2(-direction.y, -direction.x).angle(), delta * 10)
+		
+		$Camera.transform.origin = $Camera.transform.origin.linear_interpolate(transform.origin + camera_init_pos, delta * 15)
+		if direction:
+			rotation.y = lerp_angle(rotation.y, Vector2(-direction.y, -direction.x).angle(), delta * 10)
 
-	if pushing and direction:
-		$Mesh/AnimationPlayer.play("push")
-	elif velocity.length() > 3:
-		$Mesh/AnimationPlayer.play("walk", -1, 2)
-	else:
-		$Mesh/AnimationPlayer.play("idle")
+		if pushing and direction:
+			$Mesh/AnimationPlayer.play("push")
+		elif velocity.length() > 3:
+			$Mesh/AnimationPlayer.play("walk", -1, 2)
+		else:
+			$Mesh/AnimationPlayer.play("idle")
 
 func _input(event):
 	if event.is_action_pressed("action"):
@@ -89,6 +91,22 @@ func unbarrel():
 	$BarrelCollisionShape.disabled = true
 	barrel_mode = false
 	emit_signal("unbarreled")
+
+func hide_in_manhole(manhole: Spatial):
+	if not barrel_mode:
+		$CollisionShape.disabled = true
+		var dir = global_translation.direction_to(manhole.global_translation)
+		rotation.y = atan2(-dir.z, dir.x) - PI / 2
+		global_translation = manhole.global_translation - dir * 1.1
+		$Mesh/AnimationPlayer.play("cat_hide")
+		manholed = true
+	return not barrel_mode
+
+func unmanhole():
+	$Mesh/AnimationPlayer.play_backwards("cat_hide")
+	yield($Mesh/AnimationPlayer, "animation_finished")
+	$CollisionShape.disabled = false
+	manholed = false
 
 func _on_timeout():
 	if not Global.intro:
